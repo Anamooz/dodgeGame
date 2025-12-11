40a96bff137f58d658772943a8a8bbec51c19f62
Anamooz <145793103+Anamooz@users.noreply.github.com>
2025-12-11 12:42:43 -0800

    added left and right movement

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+    window.setFramerateLimit(60);
 
     // Load texture (sprite idle sheet)
     sf::Texture texture;
@@ -13,7 +14,7 @@ int main()
         return -1;
     }
 
-    // Sprite setup
+    // ------Sprite setup--------
     sf::Sprite sprite(texture);
     sprite.setTextureRect(sf::IntRect{ {0, 0}, {32, 32} });
     sprite.setOrigin({16.f, 16.f});
@@ -25,10 +26,14 @@ int main()
     const int frameHeight = 32;
 
     int currentFrame = 0;
-    float frameTime = 0.1f;         // seconds per frame
-    float timer = 0.f;
+    float frameTimer = 0.f;
+    float frameSpeed = 0.1f;   // seconds per frame
 
     sf::Clock clock;
+
+    // ------Movement setup--------
+    float speed = 200.f;       // pixels per second
+    bool facingLeft = false;
 
     while (window.isOpen()){
         while (auto event = window.pollEvent()){
@@ -37,19 +42,50 @@ int main()
         }
 
         float dt = clock.restart().asSeconds();
-        timer += dt;
 
-        // Update animation
-        if (timer >= frameTime)
+         // --- Movement Input ---
+        bool moving = false;
+
+        // Move left
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A) ||
+            sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
         {
-            timer = 0.f;
-            currentFrame = (currentFrame + 1) % frameCount;
-
-            sprite.setTextureRect(sf::IntRect{
-                {currentFrame * frameWidth, 0},
-                {frameWidth, frameHeight}
-            });
+            sprite.move({-speed * dt, 0.f});
+            facingLeft = true;
+            moving = true;
         }
+
+        // Move right
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) ||
+            sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        {
+            sprite.move({speed * dt, 0.f});
+            facingLeft = false;
+            moving = true;
+        }
+
+        // Flip sprite by using scale
+        sprite.setScale({facingLeft ? -1.f : 1.f, 1.f});
+
+        // --- ANIMATION ---
+                if (moving){
+                    frameTimer += dt;
+                    if (frameTimer >= frameSpeed){
+                        frameTimer = 0.f;
+                        currentFrame = (currentFrame + 1) % frameCount;
+
+                        sprite.setTextureRect(sf::IntRect{
+                            {currentFrame * frameWidth, 0},
+                            {frameWidth, frameHeight}
+                        });
+                    }
+                }
+                else
+                {
+                    // Idle frame (frame 0)
+                    currentFrame = 0;
+                    sprite.setTextureRect(sf::IntRect{ {0, 0}, {32, 32} });
+                }
 
         // Rendering
         window.clear();
